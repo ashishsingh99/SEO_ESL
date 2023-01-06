244fe5f0e7254114db3efba66d9beaf82083258f
Ashish Kumar <51400448+ashishsingh99@users.noreply.github.com>
2023-01-06 12:02:37 +0530

    Update constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -6,14 +6,14 @@
 // export const Country = () => `https://api.dataforseo.com/v3/serp/google/locations`;
 
 
-export const login_withAPi = () => `https://a10a-2405-201-4041-c851-ac6c-a2cd-41ae-5697.in.ngrok.io/api/user/login/`;
-export const regester_withAPi = () => `https://a10a-2405-201-4041-c851-ac6c-a2cd-41ae-5697.in.ngrok.io/api/user/register/`;
-export const forgot_withApi = () => `https://a10a-2405-201-4041-c851-ac6c-a2cd-41ae-5697.in.ngrok.io/api/user/send-reset-password-email/`;
+export const login_withAPi = () => `http://43.204.238.20:8000/api/user/login/`;
+export const regester_withAPi = () => `http://43.204.238.20:8000/api/user/register/`;
+export const forgot_withApi = () => `http://43.204.238.20:8000/api/user/send-reset-password-email/`;
 export const profile_withApi = () => `http://43.204.238.20:8000/api/user/profile/`;
 export const taskPost_withApi = () => `https://api.dataforseo.com/v3/serp/google/organic/task_post`;
 export const ranktracking_withAPi = () => `https://api.dataforseo.com/v3/serp/google/organic/task_get/regular/$`;
 export const Country = () => `https://api.dataforseo.com/v3/serp/google/locations`;
-export const keywordId_withApi = () => `https://a10a-2405-201-4041-c851-ac6c-a2cd-41ae-5697.in.ngrok.io/api/user/KeywordId/`;
+export const keywordId_withApi = () => `http://43.204.238.20:8000/api/user/KeywordId/`;
 
 
 
